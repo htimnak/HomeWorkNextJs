6e65306963a9c29b2a4b35c69e5d70a30e149623
hi <astitmnak1@gmial.com>
2023-11-29 17:51:22 +0330

    this is simple Component for learn how to create component

---
+++ src/components/MyTable.tsx
@@ -0,0 +1,29 @@
+export default function MyTable(){
+    return (
+        <>
+            <table>
+                <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+
+                </tr>
+                </tbody>
+            </table>
+            <table>
+                <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+
+                </tr>
+                </tbody>
+            </table>
+        </>
+
+    )
+
+}
+//export default MyTable;
