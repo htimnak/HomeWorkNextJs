f0b6c74785a351114e935f27f37b22128996aadb
hi <astitmnak1@gmial.com>
2024-02-01 12:31:58 +0330

    add style to from

--- src/components/AddForm.tsx
+++ src/components/AddForm.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function AddForm(props) {
     return (
-       <form className={"mb-4"}>
+       <form className={"mb-4 p-4 bg-gray-50 shadow"}>
            <input className={"border mr-8 rounded px-4  py-2 bg-transparent hover:border-green-200 hover:shadow  focus:outline-none focus:ring focus:ring-green-200"} type={"text"} name={"name"} placeholder={"name"}/>
            <input className={"border mr-8 rounded px-4  py-2 bg-transparent hover:border-green-200  hover:shadow focus:outline-none focus:ring focus:ring-green-200"} type={"number"} name={"price"} placeholder={"3000"}/>
-           <input className={"bg-pink-200 px-4 py-2 rounded text-white hover:shadow"} type={"submit"} value={"add product"}/>
+           <input className={"bg-pink-200 px-4 py-2 rounded text-gray-500 hover:shadow"} type={"submit"} value={"add product"}/>
        </form>
     );
 }
