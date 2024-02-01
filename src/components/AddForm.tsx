d9fe46bb90ac80122c31210edf672207f29b21b0
hi <astitmnak1@gmial.com>
2024-02-01 12:10:03 +0330

    add style to input

--- src/components/AddForm.tsx
+++ src/components/AddForm.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function AddForm(props) {
     return (
-       <form>
-           <input type={"text"} name={"name"} placeholder={"name"}/>
-           <input type={"number"} name={"price"} placeholder={"3000"}/>
+       <form className={"mb-4"}>
+           <input className={"border mr-8 rounded px-4  py-2 bg-transparent"} type={"text"} name={"name"} placeholder={"name"}/>
+           <input className={"border mr-8 rounded px-4  py-2 bg-transparent"} type={"number"} name={"price"} placeholder={"3000"}/>
            <input type={"submit"} value={"add product"}/>
        </form>
     );
