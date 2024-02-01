72b1f948187076e510c6a40866f3bef6df17b12a
hi <astitmnak1@gmial.com>
2024-02-01 18:40:52 +0330

    add nameState and priceState value

--- src/components/AddForm.tsx
+++ src/components/AddForm.tsx
@@ -8,20 +8,25 @@ export default function AddForm({MyDataState,SetMyDataState}) {
         setNameState(e.target.value);
     }
     const ChangePriceHandler=(e)=>{
-        setPriceState(e.target.value)
+        setPriceState(e.target.value);
     }
     const SubmitHandler = (e)=>{
         e.preventDefault();
-        formData.ID= Math.random();
-        console.log(formData);
-        SetMyDataState([...MyDataState,formData]);
+
+        SetMyDataState([...MyDataState,{
+            ID:Math.random(),
+            name: nameState,
+            price: priceState
+        }]);
+        setNameState("");
+        setPriceState("");
     }
     return (
        <form className={"mb-4 p-4 bg-gray-50 shadow"} onSubmit={SubmitHandler}>
            <input className={"border mr-8 rounded px-4  py-2 bg-transparent hover:border-green-200 hover:shadow  focus:outline-none focus:ring focus:ring-green-200"}
-                  type={"text"} name={"name"} placeholder={"name"} onChange={ChangeNameHandler}/>
+                  type={"text"} name={"name"} placeholder={"name"} value={nameState} onChange={ChangeNameHandler}/>
            <input className={"border mr-8 rounded px-4  py-2 bg-transparent hover:border-green-200  hover:shadow focus:outline-none focus:ring focus:ring-green-200"}
-                  type={"number"} name={"price"} placeholder={"3000"} onChange={ChangePriceHandler}/>
+                  type={"number"} name={"price"} placeholder={"3000"} value={priceState} onChange={ChangePriceHandler}/>
            <input className={"bg-pink-200 px-4 py-2 rounded text-gray-500 hover:shadow"}
                   type={"submit"} value={"add product"}/>
        </form>
