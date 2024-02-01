e602f6e6e9c2bc7f733bf11ecdee700512ef5555
hi <astitmnak1@gmial.com>
2024-02-01 12:13:37 +0330

    add style to input submit

--- src/components/AddForm.tsx
+++ src/components/AddForm.tsx
@@ -5,7 +5,7 @@ export default function AddForm(props) {
        <form className={"mb-4"}>
            <input className={"border mr-8 rounded px-4  py-2 bg-transparent"} type={"text"} name={"name"} placeholder={"name"}/>
            <input className={"border mr-8 rounded px-4  py-2 bg-transparent"} type={"number"} name={"price"} placeholder={"3000"}/>
-           <input type={"submit"} value={"add product"}/>
+           <input className={"bg-pink-200 px-4 py-2 rounded text-white hover:shadow"} type={"submit"} value={"add product"}/>
        </form>
     );
 }
